95a5f3709ec6cc1d72524efed4234811b97b132c
caffrey928 <caffreyacnologialawace@gmail.com>
2023-06-07 04:51:45 +0800

    [Verifier] add Tutorial & Loader

--- iota-verifier/frontend/src/App.tsx
+++ iota-verifier/frontend/src/App.tsx
@@ -1,90 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Button from "@mui/material/Button";
-import { Verify } from "./api";
 import MainDialog from "./components/main_dialog";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [files, setFiles] = useState("");
-  const [challenge, setChallenge] = useState("");
-  const [verified, setVerified] = useState("Not Verified");
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const fileReader = new FileReader();
-    const target = e.target as HTMLInputElement;
-
-    if (!target.files) return;
-    fileReader.readAsText(target.files[0], "UTF-8");
-
-    fileReader.onload = () => {
-      setFiles(fileReader.result as string);
-    };
-  };
-
-  const handleVerify = () => {
-    Verify(files, challenge).then((res) => {
-      setVerified(res);
-    });
-  };
-
-  const generate_challenge = (length: number) => {
-    const characters = 
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let result = '';
-
-    // Create an array of 32-bit unsigned integers
-    const randomValues = new Uint32Array(length);
-    
-    // Generate random values
-    window.crypto.getRandomValues(randomValues);
-    randomValues.forEach((value) => {
-      result += characters.charAt(value % charactersLength);
-    });
-
-    setChallenge(result)
-  }
-
   return (
-    <>
       <MainDialog/>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <Button variant="contained" component="label" onClick={() => generate_challenge(6)}>
-        Generate Challenge
-      </Button>
-      <p className="read-the-docs">
-        Challenge: {challenge ? challenge : "Click 'Generate Challenge' to get a random challenge!"}
-      </p>
-      <Button variant="contained" component="label">
-        Upload File
-        <input type="file" accept=".json" onChange={handleChange} hidden />
-      </Button>
-      <br />
-      <br />
-      <Button variant="outlined" onClick={handleVerify}>
-        Verify
-      </Button>
-      {<p>{verified}</p>} */}
-    </>
   );
 }
 
--- iota-verifier/frontend/src/components/main_dialog.tsx
+++ iota-verifier/frontend/src/components/main_dialog.tsx
@@ -1,51 +1,131 @@
-import { useState, useEffect } from "react";
+import * as React from 'react';
+import { useState, useEffect, CSSProperties } from "react";
 import "../css/main_dialog.css"
 import background from "../assets/background.jpg"
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import CloseIcon from '@mui/icons-material/Close';
 import { Verify } from "../api";
 import IconButton from "@mui/material/IconButton";
+import BarLoader from "react-spinners/BarLoader";
 
 const MainDialog = () => {
     const [challenge, setChallenge] = useState("");
     const [files, setFiles] = useState("");
     const [fileName, setFileName] = useState("");
     const [verified, setVerified] = useState("Not Verified");
+    const [tutorial, setTutorial] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const fileReader = new FileReader();
         const target = e.target as HTMLInputElement;
-    
+
         if (!target.files) return;
         setFileName(target.files[0].name);
         fileReader.readAsText(target.files[0], "UTF-8");
-    
+
         fileReader.onload = () => {
-          setFiles(fileReader.result as string);
+            setFiles(fileReader.result as string);
         };
     };
-    
+
     const handleVerify = () => {
+        setLoading(true);
+
         Verify(files, challenge).then((res) => {
             setVerified(res);
+            setLoading(false);
         });
     };
 
     const generate_challenge = (length: number) => {
-        const characters = 
-          'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const characters =
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         const charactersLength = characters.length;
         let result = '';
-    
+
         // Create an array of 32-bit unsigned integers
         const randomValues = new Uint32Array(length);
-        
+
         // Generate random values
         window.crypto.getRandomValues(randomValues);
         randomValues.forEach((value) => {
-          result += characters.charAt(value % charactersLength);
+            result += characters.charAt(value % charactersLength);
         });
-    
+
         setChallenge(result)
+    }
+
+    const override: CSSProperties = {
+        // display: "in-line",
+        margin: "0 auto",
+        borderColor: "white",
+    };
+
+    const Loader = () => {
+        return (
+            <>
+                <p style={{fontSize: "18px", textAlign:"center", marginBottom: "1rem"}}>Verifying...</p>
+                <BarLoader
+                    color="white"
+                    loading={loading}
+                    height={5}
+                    width={150}
+                    cssOverride={override}
+                />
+            </>
+        )
+    }   
+
+    const MainPage = () => {
+        return (
+            <>
+                <p className="dialog__text">Challenge: {challenge ? challenge : "Click 'Generate Challenge'"}</p>
+                <p className="dialog__text">File: {fileName ? fileName : "Click 'Upload Presentation File'"}</p>
+                <p className="dialog__text">Status: {loading ? <Loader/> : verified}</p>
+
+                <button className="dialog__button" onClick={() => generate_challenge(6)}>
+                    Generate Challenge
+                </button>
+                <label className="dialog__fileButton">
+                    Upload Presentation File
+                    <input type="file" accept=".json" onChange={handleChange} hidden />
+                </label>
+                <button className="dialog__button" onClick={handleVerify}>
+                    Verify
+                </button>
+            </>
+        )
+    }
+
+    const TutorialPage = () => {
+        return (
+            <>
+                <p className="tutorial__title">Step 1 : <br />  </p>
+                <p className="tutorial__text">
+                    Click "Generate Challenge" to get a random challenge.
+                    <br />
+                    <br />
+                    (Showed in "Challenge" field.)
+                </p>
+                <p className="tutorial__title">Step 2 : <br /></p>
+                <p className="tutorial__text">Go to Holder, generate a presentation file with this challenge.</p>
+                <p className="tutorial__title">Step 3 : <br /></p>
+                <p className="tutorial__text">
+                    Upload the presentation file (Click "Upload Presentation File").
+                    <br />
+                    <br />
+                    If upload successfully, you can see the file name in "File" field.
+                </p>
+                <p className="tutorial__title">Step 4 : <br /></p>
+                <p className="tutorial__text">
+                    Click "Verify" to verify your identity.
+                    <br />
+                    <br />
+                    The result would show up in "Status" field.
+                </p>
+            </>
+        )
     }
 
     useEffect(() => {
@@ -60,30 +140,19 @@ const MainDialog = () => {
     return (
         <>
             <div className="page">
-                <img src={ background } alt="background image" className="background__img"/>
+                <img src={background} alt="background image" className="background__img" />
 
                 <div className="dialog__form">
-                    <h1 className="dialog__title">Verifier</h1>
-                    <div className="dialog__iconbutton">
-                        <IconButton size="large" title="Tutorial" onClick={() => generate_challenge(6)}>
-                            <ErrorOutlineIcon className="dialog__icon"/>
-                        </IconButton>
+                    <div className="grid-container">
+                        <div />
+                        <h1 className="dialog__title">{tutorial ? "Tutorial" : "Verifier"}</h1>
+                        <div className="dialog__iconbutton">
+                            <IconButton size="large" title="Tutorial" onClick={() => setTutorial(!tutorial)}>
+                                {tutorial ? <CloseIcon className="dialog__icon" /> : <ErrorOutlineIcon className="dialog__icon" />}
+                            </IconButton>
+                        </div>
                     </div>
-                    
-                    <p className="dialog__text">Challenge: { challenge ? challenge : "Click 'Generate Challenge'"}</p>
-                    <p className="dialog__text">File: {fileName ? fileName : "Click 'Upload Presentation File'"}</p>
-                    <p className="dialog__text">Status: {verified}</p>
-
-                    <button className="dialog__button" onClick={() => generate_challenge(6)}>
-                        Generate Challenge
-                    </button>
-                    <label className="dialog__fileButton">
-                        Upload Presentation File
-                        <input type="file" accept=".json" onChange={handleChange} hidden/>
-                    </label>
-                    <button className="dialog__button" onClick={handleVerify}>
-                        Verify
-                    </button>
+                    {tutorial ? <TutorialPage /> : <MainPage />}
                 </div>
             </div>
         </>
