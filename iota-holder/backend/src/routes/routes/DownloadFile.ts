393640c269aade875a2ea596445bbe43c20eca71
Rex-Shih <53405327+Rex-Shih@users.noreply.github.com>
2023-06-15 05:53:38 +0800

    Delete DownloadFile.ts

--- iota-holder/backend/src/routes/routes/DownloadFile.ts
+++
@@ -1,6 +0,0 @@
-const DownloadFileRoute = async (req: any, res: any) => {
-  const data = req.query
-  console.log(data)
-  res.download(`./stronghold-files/${data.name}.hodl`)
-}
-export default DownloadFileRoute
