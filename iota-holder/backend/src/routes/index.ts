4cd6273fa72cda8eb795a52e5217fde42951dd69
Rex-Shih <53405327+Rex-Shih@users.noreply.github.com>
2023-06-15 05:48:19 +0800

    Update index.ts

--- iota-holder/backend/src/routes/index.ts
+++ iota-holder/backend/src/routes/index.ts
@@ -9,6 +9,7 @@ import DownloadFileRoute from "./DownloadFile";
 import uploadFileRoute from "./uploadFile";
 import downloadVPRoute from "./downloadVP";
 import checkUsrNameRoute from "./checkUsrName";
+import removeFileRoute from "./removeFile";
 const wrap =
   (fn: any) =>
   (...args: any) =>
@@ -26,6 +27,7 @@ function main(app: any) {
   app.get("/api/downloadVP", wrap(downloadVPRoute));
   app.post("/api/checkUsrName", wrap(checkUsrNameRoute));
   app.post("/api/VM", wrap(VMRoute));
+  app.post("/api/removeFile", wrap(removeFileRoute));
 }
 
 export default main;
